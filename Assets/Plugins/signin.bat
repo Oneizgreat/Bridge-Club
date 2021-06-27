b482aba9136e590421d1b9f37938946c03058049
Oneizgreat <boraristic7@gmail.com>
2021-06-28 00:10:09 +0100

    Initial commit

---
+++ Assets/Plugins/signin.bat
@@ -0,0 +1,3 @@
+echo off
+title Sign In to AltspaceVR
+curl -v -d "user[email]=boraristic7@gmail.com&user[password]=Zeznicka01" https://account.altvr.com/users/sign_in.json -c cookie
---
+++ Assets/Plugins/upload.bat
@@ -0,0 +1,2 @@
+echo off
+curl -v -b cookie -X PUT -F "space_template[zip]=@C:\Users\Bora-Ristic\Documents\GitHub\Bridge Club\template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1692728501750202977.json
